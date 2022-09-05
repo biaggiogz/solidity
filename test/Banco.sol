1dfbbdc28022bb24ec3de815febd1f8c5b51c093
Remix Workspace <>
2022-09-05 20:31:24 +0200

    global variables

---
+++ test/Banco.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.7;
+
+contract Banco {
+    uint storedData; // State variable
+    // ...
+
+    constructor (){
+
+    }
+
+    function globalvars() external view returns  (address,uint,uint)  {
+        address sender = msg.sender;
+        uint timestamp = block.timestamp;
+        uint number = block.number;
+
+        return (sender,timestamp,number);
+
+
+    }
+}
